9c8fc66671aafda6c33f2bd22ed714977a564956
carloscoss <crobertohenriquez@gmail.com>
2026-05-10 22:11:40 -0500

    Dealership class done

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,81 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    // specific getters and setters.
+    public List<Vehicle> getVehiclesByPrice(int min, int max){
+    return null;
+    }
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+    public List<Vehicle> getVehiclesByYear(int min, int max){
+        return null;
+    }
+    public List<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+    public List<Vehicle> getVehiclesByMileage(int min, int max){
+        return null;
+    }
+    public List<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+
+    //other methods
+    public List<Vehicle> getAllVehicle(){
+        return inventory;
+    }
+    public void addVehicle(Vehicle vehicle){
+        inventory.add(vehicle);
+        System.out.println("Vehicle added");
+    }
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+
+
+
+
+}
