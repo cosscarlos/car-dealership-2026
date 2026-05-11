334c32c5ff06679b8f4c54a89ca59078ae2c26af
carloscoss <crobertohenriquez@gmail.com>
2026-05-10 21:26:45 -0500

    Vehicle class done

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,77 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private int odometer;
+    private double price;
+
+    public Vehicle(double price, int odometer, String vehicleType, String model, String make, int year, int vin) {
+        this.price = price;
+        this.odometer = odometer;
+        this.vehicleType = vehicleType;
+        this.model = model;
+        this.make = make;
+        this.year = year;
+        this.vin = vin;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
