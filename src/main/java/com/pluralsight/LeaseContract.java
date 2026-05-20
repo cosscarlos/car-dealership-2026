9ec418e0295fa799fe78508fad955700686e7b89
carloscoss <crobertohenriquez@gmail.com>
2026-05-19 22:55:55 -0500

    Lease contract done

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,13 +1,33 @@
 package com.pluralsight;
 
-public abstract class LeaseContract extends Contract{
+public class LeaseContract extends Contract{
 
     private double expectedEndingValue;
-    private double leaseFee = 1.07;
-    private double monthlyPayment;
+    private double leaseFee;
 
-    public LeaseContract(String date, String customerName, String customerEmail, Vehicle sold, double expectedEndingValue, double leaseFee, double monthlyPayment) {
-        super(date, customerName, customerEmail, sold);
-        this.monthlyPayment = monthlyPayment;
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle, double expectedEndingValue, double leaseFee) {
+        super(date, customerName, customerEmail, vehicle);
+        this.expectedEndingValue = vehicle.getPrice() * 0.5;
+        this.leaseFee = vehicle.getPrice() * 0.07;
     }
+
+
+
+    @Override
+    public double getTotalPrice(){
+        return expectedEndingValue + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int months = 36;
+        double rate = 0.04;
+        double interest = getTotalPrice() * rate * (months / 12.0);
+        return (getTotalPrice() + interest) / months;
+    }
+
+    public double getExpectedEndingValue() { return expectedEndingValue; }
+    public double getLeaseFee() { return leaseFee; }
 }
+
+
